d6e13b12e9f616b553f28e144d30448d2290f940
SyedSiddiqShakir <syedsiddiqshakir@gmail.com>
2024-12-09 23:55:23 +0100

    more json prac

--- jsons/first_json.py
+++ jsons/first_json.py
@@ -1,7 +1,14 @@
 import json
 
-file = "01_names.json"
+# file = "01_names.json"
 
-with open (file, 'r') as json_file:
-    data = json.load(json_file)
-    print(data)
+def write_json(data, filename="new_json_file.json"):
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
+# with open (file, 'r') as json_file:
+#     data = json.load(json_file)
+#     write_json(data)
+
+data = ["Bob", "Marley"]
+write_json(data)
