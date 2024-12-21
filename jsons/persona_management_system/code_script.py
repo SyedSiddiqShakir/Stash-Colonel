a49043f222700e5e2ee6d7acf227b039d3baaa1d
SyedSiddiqShakir <syedsiddiqshakir@gmail.com>
2024-12-21 20:11:06 +0100

    more changes but the append thing needs work

--- jsons/persona_management_system/code_script.py
+++ jsons/persona_management_system/code_script.py
@@ -9,7 +9,7 @@ class Persona():
         self.occupation = occupation
         self.email = email
     
-    def to_dict(self):
+    def to_dict(self):                            #to convert instance information to a dictionary 
         return {
             "name": self.name,
             "age": self.age,
@@ -18,15 +18,15 @@ class Persona():
         }
     
     @classmethod
-    def from_dict(cls, data):
+    def from_dict(cls, data):                     #to convert dictionary to an instance
         return cls(data["name"], data["age"], data["email"], data["occupation"])
 
-class Person_Management():
+class Person_Management():                         #class dedicated only for managing the personas
     def __init__(self, filename="people.json"):
         self.filename = filename
         self.people = self.load_person()
     
-    def load_person(self):
+    def load_person(self):                         #loads the existing personas to json
         try:
             with open(self.filename, "r") as file:
                 loaded_people = json.load(file)
@@ -34,17 +34,25 @@ class Person_Management():
                     return Persona.to_dict(person)
         except FileNotFoundError:
             return []
-        except json.JSONDecodeError:
+        except json.JSONDecodeError:                
             print("Error decoding JSON, starting with a new list")
             return []
         
-    def save_person(self):
+    # def save_person(self):                         #saves all personas to JSON
+    #     with open(self.filename, "w") as file:
+    #         for persons in self.people:
+    #             data = Persona.to_dict(persons)    
+    #         json.dump(data, file, indent=4)
+            
+    def add_persona(self, name, age, email, occupation):
+        data = Persona.to_dict()
+        self.people.append(data)
         with open(self.filename, "w") as file:
             for persons in self.people:
-                data = Persona.to_dict(persons)    
+                    data = Persona.to_dict(persons)    
             json.dump(data, file, indent=4)
+
             
-        
             
 
     # #converts the persona object to a json string
