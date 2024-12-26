661798f6ef5bd822bc48f0919f039549ee6b02a2
SyedSiddiqShakir <syedsiddiqshakir@gmail.com>
2024-12-26 20:06:55 +0100

    more numpy prac

--- my_numpy/numpy_prac.py
+++ my_numpy/numpy_prac.py
@@ -3,3 +3,10 @@ import numpy as np
 a = np.array([1,2,3,4,5])
 print(a[:-2])
 print(type(a))
+
+b = np.array([[5,2,6,8,2],
+            [5,2,6,8,2],
+            [5,2,6,8,2],
+            [5,2,6,8,2]])
+print(b[1,2])
+print(b[2,1])
