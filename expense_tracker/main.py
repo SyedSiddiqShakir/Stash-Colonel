e24c9688dd571181ec0ff996440525b32a0a1046
SyedSiddiqShakir <syedsiddiqshakir@gmail.com>
2025-01-06 22:21:15 +0100

    new project expense tracker

---
+++ expense_tracker/main.py
@@ -0,0 +1,25 @@
+class Expense():
+    def __init__(self, amount, category, date):
+        self.amount = amount
+        self.category = category
+        self.date = date
+
+    #convert data to dictionary and stores in JSON
+    def to_dict():
+        pass
+
+    #brings json data to program variable
+    def from_dict(cls, data):
+        pass
+
+        #adds a data entry
+    def add_expense(self):
+        pass
+
+        #displays all data entries
+    def display_all_expenses(self):
+        pass
+        
+        #deletes a data entry
+    def delete_expense(self):
+        pass
