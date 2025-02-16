a3bbbf930b69bb231067688ac3759cd49770a307
SyedSiddiqShakir <syedsiddiqshakir@gmail.com>
2025-02-16 19:04:07 +0100

    checking iteration of random generators

---
+++ advanced py/random_checker.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+password_chars = [
+        random.choice(string.ascii_uppercase),  # One uppercase letter
+        random.choice(string.ascii_lowercase),  # One lowercase letter
+        random.choice(string.digits)            # One digit
+    ]
+
+print(password_chars)
+password_chars = [
+        random.choice(string.ascii_uppercase),  # One uppercase letter
+        random.choice(string.ascii_lowercase),  # One lowercase letter
+        random.choice(string.digits)            # One digit
+    ]
+
+
+print(password_chars)
+password_chars = [
+        random.choice(string.ascii_uppercase),  # One uppercase letter
+        random.choice(string.ascii_lowercase),  # One lowercase letter
+        random.choice(string.digits)            # One digit
+    ]
+
+print(password_chars)
